4bf1b10885261455dddf5733652ff59bbb230a0f
aosun1314 <aosun1314@gmail.com>
2026-05-28 14:58:01 +0800

    创建 RootListController.m

---
+++ mypreference/RootListController.m
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface PSListController : UIViewController
+@end
+
+@interface MyDebugToolPrefsListController : PSListController
+@end
+
+@implementation MyDebugToolPrefsListController
+- (id)specifiers {
+    return nil;
+}
+@end
